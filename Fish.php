7e866ad4d3e2c5d58bb4c8c674a4351f5af4efa9
Kevin Hamer <kevin@imarc.net>
2012-08-03 16:48:43 -0400

    Updating fish to be instanced

--- Fish.php
+++ Fish.php
@@ -6,10 +6,22 @@
  * @author     Kevin Hamer [kh] <kevin@imarc.net>
  * @license    MIT
  *
- * @version    0.1.1
+ * @version    0.1.2
  */
 
 class Fish {
+
+	static private $instances = array();
+	static public function instance($name='default', $instance=NULL)
+	{
+		if ($instance == NULL) {
+			if (!isset(self::$instances[$name])) {
+				throw new Exception("Tried to load uninitialized instance.");
+			}
+		} else {
+			self::$instances[$name] == $instance;
+		}
+	}
 
 	static private $symbols = array(
 		'%' => '(\d+)',
@@ -17,88 +29,73 @@ class Fish {
 		':' => '(\w+)'
 	);
 
-	static public function regexifyPattern($pattern)
+	static public function getRegexFor($syntax)
 	{
-		foreach (self::$symbols as $symbol => $sym_pattern) {
-			$pattern = preg_replace('|' . $symbol . '\w+|', $sym_pattern, $pattern);
+		foreach (self::$symbols as $symbol => $symbol_regex) {
+			$syntax = preg_replace('`' . $symbol . '\w+`', $symbol_regex, $syntax);
 		}
-		return "|$pattern|";
+		return "`$syntax`";
 	}
 
-	static public function getUsedParameters($pattern)
+	static public function getParametersFor($syntax)
 	{
-		$symbol_list = implode('', array_keys(self::$symbols));
-		preg_match_all('|[' . $symbol_list . '](\w+)|', $pattern, $matches);
+		$symbol_list = implode('|', array_keys(self::$symbols));
+		preg_match_all("`($symbol_list)(\w+)`", $syntax, $matches);
 
-		return array_combine($matches[1], $matches[0]);
+
+		return array_combine($matches[0], $matches[2]);
 	}
 
-	static public function populatePattern($pattern, $data)
+	static public function getValuesFor($syntax, $uri)
 	{
-		$parameters = self::getUsedParameters($pattern);
-		foreach ($parameters as $key => $exact) {
-			$pattern = str_replace($exact, $data[$key], $pattern);
+		$regex = self::getRegexFor($syntax);
+		preg_match_all($regex, $uri, $matches);
+		var_dump($matches);
+	}
+
+	static public function populateSyntax($syntax, $parameters)
+	{
+		$used_parameters = self::getParametersFor($syntax);
+		foreach ($used_parameters as $exact => $key) {
+			if (isset($parameters[$key])) {
+				$syntax = str_replace($exact, $parameters[$key], $syntax);
+			}
 		}
 
-		return $pattern;
+		return $syntax;
 	}
 
-	/*
-	 * Syntaxes accepted:
-	 *   * place => view
-	 *   * array(place, place, ...) => view
-	 */
-	public function map($request, $view)
+
+
+
+	/* INSTANCE */
+	private $uri_callback = array();
+
+	public function map($uri, $callback)
 	{
-		if (is_array($request)) {
-			foreach ($request as $req) {
-				$this->request_view[$req] = $view;
+		if (is_array($uri)) {
+			foreach ($uri as $sub_uri) {
+				$this->map($sub_uri, $callback);
 			}
 		} else {
-			$this->request_view[$request] = $view;
+			$this->uri_callback[$uri] = $view;
 		}
+
 		return $this;
 	}
 
-	private $request_view = array();
-
-	public function go($pattern=NULL)
+	public function go($uri=NULL)
 	{
-		foreach ($this->request_view as $request_pattern => $view_callback) {
-			$regex = self::regexifyPattern($request_pattern);
-			if (preg_match($regex, $pattern)) {
-				echo "$pattern goes to $view_callback\n";
-				return $view_callback;
-			}
+		if (empty($uri)) {
+			$uri = $_SERVER['REQUEST_URI'];
 		}
-	}
 
-	public function link($data, $view)
-	{
-		foreach ($this->request_view as $request_pattern => $view_callback) {
-			$regex = self::regexifyPattern($view_callback);
-			if (preg_match($regex, $view)) {
-				$used_params = self::getUsedParameters($request_pattern);
-				if (count($used_params) <= count(array_intersect_key($used_params, $data))) {
-					$populated = self::populatePattern($request_pattern, $data);
-					echo json_encode($data) . "(view=$view) links to $populated\n";
-					return $populated;
-				}
-			}
+		foreach ($this->uri_callback as $current_uri => $current_callback) {
+			$uri_regex = self::getRegexFor($current_uri);
+			if (preg_match($regex, $uri)) {
+				$required_params = self::getParametersFor($current_uri);
+				if (count($required_params) <= count(array_intersect($required_params, array_keys($params)))) {
+					$
 		}
 	}
 }
-
-/* Tests */
-$fish = new Fish();
-
-$fish
-	->map(['/users/%id/@method', '/users/%id', '/users'],  'Users')
-	->map('/portfolio',                                    'Portfolio')
-	->map('/process',                                      'Process');
-
-$fish->go('/users/23');
-
-$fish->link([], 'Users');
-$fish->link(['id' => 42], 'Users');
-$fish->link(['id' => 42, 'method' => 'party'], 'Users');
